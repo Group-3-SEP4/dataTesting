6254096efe24b27949de90637dc0824506448411
Niklas Krogh <krogh@hotmail.dk>
2020-12-07 09:43:10 +0100

    test(unitTestTemperature) : Added the unit testing for temperature and changed the testconnection to be less duplicate code

--- UnitTestTemperatureAPI.cs
+++ UnitTestTemperatureAPI.cs
@@ -1,30 +1,29 @@
+﻿using System;
+using System.Net.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
-using System;
-using System.Net.Http;
 using Assert = NUnit.Framework.Assert;
 
 namespace WebServiceTest
 {
     [TestClass]
-    public class UnitTestCo2Api
+    public class UnitTestTemperatureAPI
     {
         [Test]
         public void TestConnection()
         {
             UnitTestConnection unitTestConnection = new UnitTestConnection();
-            unitTestConnection.TestConnection("CO2");
+            unitTestConnection.TestConnection("Temperature");
         }
 
-        //TODO will need changing after adding user to get specific device/room co2 
         [Test]
-        public void TestGetCo2()
+        public void TestGetTemperature()
         {
             using (var client = new HttpClient())
             {
-                String URL = "https://localhost:5001/CO2";
+                String URL = "https://localhost:5001/Temperature";
                 var response = client.GetStringAsync(URL);
-                Assert.IsTrue(Int16.Parse(response.Result) < 5000 && Int16.Parse(response.Result) > 410);
+                Assert.IsTrue(Int16.Parse(response.Result) == 32767);
             }
         }
     }
